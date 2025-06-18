d3cc01fdf9f4f256cbd9f654f34bbadf69d6ae3f
ElivanilsoAK <elivanilsojuniorlevita@gmail.com>
2025-06-17 21:59:50 -0300

    Envio BLog
    BLog

--- projeto1/votacao/urls.py
+++ projeto1/votacao/urls.py
@@ -3,8 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('/index', views.index, name='index'),
-    path('detalhe/<int:id_questao>', views.detalhe, name='detalhe'),
-    path('votar/<int:id_questao>', views.votar, name='votar'),
-    path('resultado/<int:id_questao>', views.resultado, name='resultado'),
-]
+    path('index/', views.index, name='index'),
+    path('<int:id_questao>/', views.detalhe, name='detalhe'),
+    path('<int:id_questao>/votar/', views.votar, name='votar'),
+    path('<int:id_questao>/resultado/', views.resultado, name='resultado'),
+]  
